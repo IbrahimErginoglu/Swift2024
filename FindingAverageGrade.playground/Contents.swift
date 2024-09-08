bbd52645eaca809078e79e64ab1924c25ef1d004
ibrahimerginoglu <ibrahim.erginoglu@hisarschool.k12.tr>
2024-09-08 13:50:14 +0300

    FindingAverageGrade created

---
+++ FindingAverageGrade.playground/Contents.swift
@@ -0,0 +1,13 @@
+let grade1 = 70
+let grade2 = 40
+
+
+let average = (grade1 + grade2) / 2
+
+
+if average >= 50 {
+    print("Your avergae grade is \(average). You have Passed")
+} else {
+    print("Your avergae grade is \(average). You have Failed")
+}
+
