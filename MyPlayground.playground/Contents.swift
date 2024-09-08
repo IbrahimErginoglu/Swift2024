e7c8253e116cf19bf0f677ff8d6b66ad54c32e3b
ibrahimerginoglu <ibrahim.erginoglu@hisarschool.k12.tr>
2024-09-08 13:16:59 +0300

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+var hava="rain"
+if hava=="rain" {
+    print("Yanına şemsiye al")
+}
+
+
+else if hava=="sunny" {
+    print("yanına gözlük al")
+}
+
+
+else if hava=="snowy" {
+    print("yanına eldiven al")
+}
+
+
+else {"Yanına bişey almana gerek yok"}
+
